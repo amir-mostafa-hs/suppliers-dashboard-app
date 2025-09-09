45afb1b80f7fa4eaefb086d241428e03c21a8bb0
Amir Mostafa Haji Sadeghian <amir-mostafa-hs@outlook.com>
2025-09-09 11:02:16 +0330

    add new url for cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,7 +17,7 @@ const port = SECRET_VARIABLES.port;
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://localhost:5173"], // Allow both Vite default ports
     credentials: true,
   })
 );
